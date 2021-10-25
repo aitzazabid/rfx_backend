d25ff918f858dd4212d274f6b965adf3f1b2aeb3
AtizazAbid <aitzaz.abid@gmail.com>
2021-10-25 22:47:26 +0500

    Updated the user profile serialize --added the firstname --added the lastname --added the email

--- core/serializers.py
+++ core/serializers.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+    def get_last_name(self, obj):
+        return obj.user.last_name
+    def get_email(self, obj):
+        return obj.user.email
+
     class Meta:
         model = UserProfile
         fields = "__all__"
